d8ae9309ff8a2135a9b33822a1c61319ec9994c4
arianna-caow <66382962+arianna-caow@users.noreply.github.com>
2020-07-13 14:54:22 -0700

    got rid of some trashy stuffs

--- readCovid.py
+++ readCovid.py
@@ -3,11 +3,18 @@ import time
 import csv
 import pandas as pd
 from twarc import Twarc
+
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 start_date = date(2020, 3, 20)
 end_date = date(2020, 7, 13) #end date, datetime.date(datetime.now()) (this second option is dynamic and changes by date but depends on timezone)
+
+OAUTH_TOKEN = "1029186921438883845-AQjxqWPxZlURJ47eWFqRFRkSCkDPFh"
+OAUTH_TOKEN_SECRET = "YgxeTz31ItxBrJubvwZpZaqa57LLhWRKLMM4t82pdEtsv"
+CONSUMER_KEY = "Y70ckEEL2TdQzyq9NqI5RriiB"
+CONSUMER_SECRET = "YWQJJlJyzXxkaPXCEdFrANgHFf4Dyd0PtkT4f5TvXFUJLUtpvU"
+
 for date in daterange(start_date, end_date):
     after = date + timedelta(1)
     print(date.strftime("%B%-d").lower()+"_"+after.strftime("%B%-d").lower()+".csv")
@@ -20,16 +27,20 @@ for date in daterange(start_date, end_date):
                 print (row)
                 break    
             """
-            OAUTH_TOKEN = "1029186921438883845-AQjxqWPxZlURJ47eWFqRFRkSCkDPFh"
-            OAUTH_TOKEN_SECRET = "YgxeTz31ItxBrJubvwZpZaqa57LLhWRKLMM4t82pdEtsv"
-            CONSUMER_KEY = "Y70ckEEL2TdQzyq9NqI5RriiB"
-            CONSUMER_SECRET = "YWQJJlJyzXxkaPXCEdFrANgHFf4Dyd0PtkT4f5TvXFUJLUtpvU"
-            dataframe=pd.read_csv(filename, header=None)
-            dataframe=dataframe[0]
-            dataframe.to_csv("ready_corona_tweets"+ date.strftime("%B%-d").lower() +".txt", index=False, header=None)
+            
+            totaldata=pd.read_csv(filename, header=None)
+            #for i in totaldata:
+            dataframe=totaldata[0]
+            print (dataframe)
+            numberfile = "number_corona_tweets"+ date.strftime("%B%-d").lower() +".txt"
+            readyfile = "ready_corona_tweets"+ date.strftime("%B%-d").lower() +".txt"
+            dataframe.to_csv(numberfile, index=False, header=None)
             t = Twarc(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-            for tweet in t.hydrate(open("ready_corona_tweets"+ date.strftime("%B%-d").lower() +".txt")):
-                print(tweet["full_text"])
+            for tweet in t.hydrate(open(numberfile)):
+                wr = open(readyfile, 'w')
+                wr.write(tweet["full_text"])
+                #print(tweet["full_text"])
+    break
 
 '''
 open()
