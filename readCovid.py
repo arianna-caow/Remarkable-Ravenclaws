4737942fa4c66392f05968f24e85c8dc67790787
arianna-caow <66382962+arianna-caow@users.noreply.github.com>
2020-07-14 09:10:27 -0700

    update

--- readCovid.py
+++ readCovid.py
@@ -3,6 +3,8 @@ import time
 import csv
 import pandas as pd
 from twarc import Twarc
+#from geopy.geocoders import Nominatim
+#import countries
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
@@ -14,6 +16,9 @@ OAUTH_TOKEN = "1029186921438883845-AQjxqWPxZlURJ47eWFqRFRkSCkDPFh"
 OAUTH_TOKEN_SECRET = "YgxeTz31ItxBrJubvwZpZaqa57LLhWRKLMM4t82pdEtsv"
 CONSUMER_KEY = "Y70ckEEL2TdQzyq9NqI5RriiB"
 CONSUMER_SECRET = "YWQJJlJyzXxkaPXCEdFrANgHFf4Dyd0PtkT4f5TvXFUJLUtpvU"
+t = Twarc(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+#cc = countries.CountryChecker('TM_WORLD_BORDERS-0.3.shp')
 
 for date in daterange(start_date, end_date):
     after = date + timedelta(1)
@@ -27,24 +32,34 @@ for date in daterange(start_date, end_date):
                 print (row)
                 break    
             """
+
             
             totaldata=pd.read_csv(filename, header=None)
-            #for i in totaldata:
             dataframe=totaldata[0]
-            print (dataframe)
             numberfile = "number_corona_tweets"+ date.strftime("%B%-d").lower() +".txt"
-            readyfile = "ready_corona_tweets"+ date.strftime("%B%-d").lower() +".txt"
+            readyfile = "ready_corona_tweets"+ date.strftime("%B%-d").lower() +".csv"
             dataframe.to_csv(numberfile, index=False, header=None)
-            t = Twarc(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
             for tweet in t.hydrate(open(numberfile)):
-                wr = open(readyfile, 'w')
-                wr.write(tweet["full_text"])
+                #print (tweet["place"]["country"])
+                if ("place" not in tweet):
+                    continue
+                if ("country" not in tweet["place"]):
+                    #print ("sdjfksldf")
+                    continue
+                if (tweet["place"]["country"] == "United States"):
+                    #print("sdkjf")
+                    with open (readyfile, 'a', newline='') as csvfile:
+                        csvwriter = csv.writer(csvfile, delimiter=' ', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
+                        csvwriter.writerow([tweet["full_text"]])
+                    #wr = open(readyfile, 'a')
+                    #wr.write(tweet["full_text"])
                 #print(tweet["full_text"])
+                #break
+            
     break
 
 '''
 open()
-
 with open('eggs.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 '''
